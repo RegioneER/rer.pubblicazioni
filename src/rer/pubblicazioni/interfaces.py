e51049eea4dbc26ad6b2263322163b9f4793e517
Giacomo Monari <arsenico13@hotmail.it>
2018-01-11 15:18:53 +0100

    [dev][new] schema for the Pubblicazione content (WIP)

--- src/rer/pubblicazioni/interfaces.py
+++ src/rer/pubblicazioni/interfaces.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
+from plone.app.textfield import RichText
 from rer.pubblicazioni import _
 from zope import schema
+from plone.namedfile.field import NamedFile
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
@@ -13,12 +15,87 @@ class IRerPubblicazioniLayer(IDefaultBrowserLayer):
 
 class IPubblicazione(Interface):
 
-    title = schema.TextLine(
-        title=_(u'Title'),
+    abstract = RichText(
+        title=_(u'rer_description_abstract', default=u'Description/Abstract'),
+        description=_(
+            u'help_rer_description_abstract',
+            default=u'An abstract of the publication.'
+        ),
+        required=False,
+    )
+
+    publicationDate = schema.Datetime(
+        title=_(u'rer_publication_date', default=u'Date'),
+        description=_(
+            u'help_rer_publication_date',
+            default=u'Insert the date for this publication'
+        ),
         required=True,
     )
 
-    description = schema.Text(
-        title=_(u'Description'),
+    # TODO - schema.List
+    publicationType = schema.Text(
+        title=_(u'rer_publication_type', default=u'Type - TODO'),
+        description=_(
+            u'help_rer_publication_type',
+            default=u'Insert a list of types for this publication'
+        ),
+        required=False,
+    )
+
+    publicationLanguage = schema.Choice(
+        title=_(u'rer_publication_language', default=u'Language'),
+        description=_(
+            u'help_rer_publication_language',
+            default=u'Select the language of this publication'
+        ),
+        required=False,
+        default=None,
+        vocabulary="rer.pubblicazioni.lingue"
+    )
+
+    # "Pubblicato in" - era "Collana"
+    publicationSeries = schema.Text(
+        title=_(u'rer_publication_series', default=u'Series - TODO'),
+        description=_(
+            u'help_rer_publication_series',
+            default=u''
+        ),
+        required=False,
+    )
+
+    publicationEditor = schema.Text(
+        title=_(u'rer_publication_editor', default=u'Editor'),
+        description=_(
+            u'help_rer_publication_editor',
+            default=u''
+        ),
+        required=False,
+    )
+
+    publicationRights = RichText(
+        title=_(u'rer_publication_rights', default=u'Copyrights'),
+        description=_(
+            u'help_rer_rer_publication_rights',
+            default=u''
+        ),
+        required=False,
+    )
+
+    publicationFile = NamedFile(
+        title=_(u'rer_publication_file', default=u'File'),
+        description=_(
+            u'help_rer_publication_file',
+            default=u''
+        ),
+        required=False,
+    )
+
+    publicationURL = schema.Text(
+        title=_(u'rer_publication_url', default=u'URL'),
+        description=_(
+            u'help_rer_publication_url',
+            default=u''
+        ),
         required=False,
     )
