ee590fd15c72f415984e43af8744961730e0b147
Andrea Cecchi <andrea.cecchi85@gmail.com>
2019-11-14 13:06:01 +0100

    Back to development: 2.2.4

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='rer.pubblicazioni',
-    version='2.2.3',
+    version='2.2.4.dev0',
     description="An add-on to manage publications for Regione Emilia Romagna.",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
